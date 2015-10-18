04d8d563f8ffe1837f245394501e5b9a2c6ef24a
Ilya Makarov <ilmaksad@gmail.com>
2015-10-18 21:22:56 +0600

    test123 add std::

--- MolecularDynamics/classes.cpp
+++ MolecularDynamics/classes.cpp
@@ -5,7 +5,7 @@
 const double Molecule::r = Angstrom;	//m
 const double Molecule::m = 20.1797 * AtomicMassUnit;	//kg
 const double Molecule::sigma = 2.74 * Angstrom;		//m
-const double Molecule::epsilon = 10.0 * 36.2 * Boltzmann;	//J
+const double Molecule::epsilon = /*10.0 * */ 36.2 * Boltzmann;	//J
 
 
 
@@ -13,19 +13,19 @@ const double Molecule::epsilon = 10.0 * 36.2 * Boltzmann;	//J
 Space::Space(int width, int height, int n)
 	:width(width), height(height)
 {
-	srand(time(nullptr));
-	molecules.resize(n);
+	std::srand(std::time(nullptr));
+	molecules.std::resize(n);
 	for (auto &i : molecules) {
 		Molecule m;
-		m.x = rand() % width;
+		m.x = std::rand() % width;
 		m.x *= Angstrom;
-		m.y = rand() % height;
+		m.y = std::rand() % height;
 		m.y *= Angstrom;
-		double v = 300 + (rand() % 200 - 100);
-		double alphaDeg = rand() % 360;
+		double v = 300 + (std::rand() % 200 - 100);
+		double alphaDeg = std::rand() % 360;
 		double alpha = alphaDeg / 360.0 * 2 * pi;
-		m.vx = v * cos(alpha);
-		m.vy = v * sin(alpha);
+		m.vx = v * std::cos(alpha);
+		m.vy = v * std::sin(alpha);
 		i = m;
 	}
 }
@@ -41,7 +41,7 @@ void Calculator::oneStep(Space &space)
 			double dF = Force(space.molecules[i], space.molecules[j]);
 			double dx = space.molecules[i].x - space.molecules[j].x;
 			double dy = space.molecules[i].y - space.molecules[j].y;
-			double hip = sqrt(dx*dx + dy*dy);
+			double hip = std::sqrt(dx*dx + dy*dy);
 			double dFx = dF * dx / hip;
 			double dFy = dF * dy / hip;
 			space.molecules[i].Fx += dFx;
@@ -70,8 +70,8 @@ void Calculator::oneStep(Space &space)
 
 double Calculator::Force(Molecule &m1, Molecule &m2)
 {
-	double r = sqrt(m1.x*m1.x + m1.y*m2.y);
-	double U = pow(Molecule::sigma / r, 12) - pow(Molecule::sigma / r, 6);
+	double r = std::sqrt(m1.x*m1.x + m1.y*m2.y);
+	double U = std::pow(Molecule::sigma / r, 12) - std::pow(Molecule::sigma / r, 6);
 	U *= 4 * Molecule::epsilon;
 	return - U / r;
 }
