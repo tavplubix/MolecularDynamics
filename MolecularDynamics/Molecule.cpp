4f5f6d9122222ab9fcb1127117320db941abefec
tavplubix <tavplubix@gmail.com>
2016-02-09 20:03:09 +0600

    Molecule.cpp fixed

---
+++ MolecularDynamics/Molecule.cpp
@@ -0,0 +1,8 @@
+#include "Molecule.h"
+
+//Ne
+const double Molecule::radius = Angstrom;	//m
+const double Molecule::m = 20.1797 * AtomicMassUnit;	//kg
+const double Molecule::sigma = 2.74 * Angstrom;		//m
+const double Molecule::epsilon = 1.0 * 36.2 * Boltzmann;	//J
+
